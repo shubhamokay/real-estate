0bbd31db525854d745adbbb6ae0b7ab8748cbfdb
Shubham Sangale <shubhamsangale2763@gmail.com>
2024-03-24 13:08:32 +0530

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg"
+    },
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFO-7LJjX67RutlPg11pieVorR3iibtwk",
+  authDomain: "real-estate-33e1a.firebaseapp.com",
+  projectId: "real-estate-33e1a",
+  storageBucket: "real-estate-33e1a.appspot.com",
+  messagingSenderId: "888992882523",
+  appId: "1:888992882523:web:db05e7b811cf6768e1447e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
