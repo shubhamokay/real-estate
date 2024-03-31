fa3775924d9ada6bb89f02ffc4b87e236c80f993
Shubham Sangale <shubhamsangale2763@gmail.com>
2024-03-31 20:02:13 +0530

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -278,7 +278,9 @@ const Profile = () => {
                 >
                   Delete
                 </button>
+                <Link to={`/update-listing/${listing._id}`}>
                 <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
