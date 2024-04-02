b01994c746eb7b4bb2e9ff0941d28c7d95d8dc97
Shubham Sangale <shubhamsangale2763@gmail.com>
2024-04-02 10:34:46 +0530

    complete listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -299,7 +299,9 @@ const CreateListing = () => {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs">(₹ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>(₹ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -316,7 +318,9 @@ const CreateListing = () => {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">(₹ / month)</span>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>(₹ / month)</span>
+                  )}
                 </div>
               </div>
             )}
