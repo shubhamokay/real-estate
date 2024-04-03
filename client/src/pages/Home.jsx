d6fe9563b7d948ff7f302f0ba99413a5bbb409c7
Shubham Sangale <shubhamsangale2763@gmail.com>
2024-04-03 11:30:38 +0530

    complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,158 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css/bundle";
+import ListingItem from "../components/ListingItem";
 
 const Home = () => {
+  SwiperCore.use([Navigation]);
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  console.log(offerListings);
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?offer=true&limit=4");
+        const data = await res.json();
+        setOfferListings(data);
+        fetchRentListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchRentListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?type=rent&limit=4");
+        const data = await res.json();
+        setRentListings(data);
+        fetchSaleListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?type=sale&limit=4");
+        const data = await res.json();
+        setSaleListings(data);
+      } catch (error) {
+        log(error);
+      }
+    };
+    fetchOfferListings();
+  }, []);
+
   return (
     <div>
-      Home
-    </div>
-  )
-}
+      {/* top */}
+      <div className="flex flex-col gap-6 py-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span> <br />
+          place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Welcome to Estate Ease, your gateway to exquisite living spaces and
+          investment opportunities.
+          <br />
+          Explore our collection of exceptional properties and start your
+          journey to a new beginning.
+        </div>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+        >
+          Let's get started...
+        </Link>
+      </div>
 
-export default Home
+      {/* swiper */}
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide>
+              <div
+                className="h-[500px]"
+                key={listing._id}
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* listing results for offer, sale and rent */}
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+        {offerListings && offerListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Offers
+              </h2>
+              <Link
+                to={"/search?offer=true"}
+                className="text-sm text-blue-800 hover:underline"
+              >
+                Show more offers
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {offerListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id}></ListingItem>
+              ))}
+            </div>
+          </div>
+        )}
+        {rentListings && rentListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Places for rent
+              </h2>
+              <Link
+                to={"/search?type=rent"}
+                className="text-sm text-blue-800 hover:underline"
+              >
+                Show more places for rent
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {rentListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id}></ListingItem>
+              ))}
+            </div>
+          </div>
+        )}
+        {saleListings && saleListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent places for sale
+              </h2>
+              <Link
+                to={"/search?type=sale"}
+                className="text-sm text-blue-800 hover:underline"
+              >
+                Show more places for sale
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {saleListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id}></ListingItem>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
